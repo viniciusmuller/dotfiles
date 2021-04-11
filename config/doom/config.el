d531f35b831ab2506ce7acb0cb6c3c5957cec6e6
arcticlimer <vinigm.nho@gmail.com>
2021-04-11 16:53:20 -0300

    Update doom emacs config files

--- config/doom/config.el
+++ config/doom/config.el
@@ -3,11 +3,23 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 ;; (setq user-full-name "John Doe"
 ;;       user-mail-address "john@doe.com")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;-----------------------------------------------------;
+; Doom configutarion                                  ;
+;-----------------------------------------------------;
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,23 +31,12 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Jetbrains Mono" :size 13 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
-;; (setq doom-font (font-spec :family "Jetbrains Mono" :size 13))
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
+(custom-set-faces!
+   (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
+   (set-face-attribute 'font-lock-type-face nil :slant 'italic))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -54,18 +55,96 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(setq-default indent-tabs-mode nil)
+(setq scroll-margin 5)
+
+;-----------------------------------------------------;
+; Helper functions                                    ;
+;-----------------------------------------------------;
+
+;; (defun create-keywords (keyword-list)
+;;   (mapcar (lambda (keyword) keyword . 'font-lock-keyword) keyword-list))
+
+;-----------------------------------------------------;
+; Package configuration                               ;
+;-----------------------------------------------------;
+
 ;; Remove evil escape key sequence
-(setq evil-escape-key-sequence nil)
+(after! evil
+  (setq evil-escape-key-sequence nil))
+
+(after! word-wrap
+  (+global-word-wrap-mode t))
 
 (use-package! company
   :config
-  (setq company-idle-delay 0.1)
-  company-minimum-prefix-length 1)
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 1))
 
 (use-package! which-key
   :config
   (which-key-setup-side-window-right)
   (setq which-key-idle-delay 0.2
-        which-key-idle-secondary-delay 0.2
-        )
-  )
+        which-key-idle-secondary-delay 0.2))
+
+(use-package! ligatures
+  :config
+  ;; Add ligatures on the programming modes
+  ;; with `set-ligatures!` and use them here
+  (plist-put! +ligatures-extra-symbols
+        ;; org
+        :name          "»"
+        :src_block     "»"
+        :src_block_end "«"
+        :quote         "“"
+        :quote_end     "”"
+        ;; Functional
+        :lambda        "λ"
+        :def           "ƒ"
+        :map           "↦"
+        ;; Type
+        :null          "∅"
+        :int           "ℤ"
+        :float         "ℝ"
+        :str           "𝕊"
+        :bool          "𝔹"
+        :list          "𝕃"
+        :true          "𝕋"
+        :false         "𝔽"
+        ;; Flow
+        :not           "￢"
+        :in            "∈"
+        :not-in        "∉"
+        :and           "∧"
+        :or            "∨"
+        :for           "∀"
+        :some          "∃"
+        :return        "⟼"
+        :yield         "⟻"
+        ;; Other
+        :union         "∪"
+        :intersect     "∩"
+        :tuple         "⨂"
+        :dot           "•"))
+
+;-----------------------------------------------------;
+; Modes configuration                                 ;
+;-----------------------------------------------------;
+
+;; (after! elixir-mode
+;;   (setq elixir-custom-keywords '("test" "describe" "setup"))
+;;   ;; Set some custom ligatures
+;;   (set-ligatures! 'elixir-mode
+;;     :not "not" :not-in "not in"
+;;     :lambda "fn" :null "nil")
+;;   ;; Add custom keywords to syntax highlighting
+;;   (font-lock-add-keywords 'elixir-mode
+;;     (create-keywords elixir-custom-keywords)))
+
+;-----------------------------------------------------;
+; Org Mode                                            ;
+;-----------------------------------------------------;
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
--- config/doom/init.el
+++ config/doom/init.el
@@ -20,41 +20,41 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +tng)      ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +icons)        ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
-       ;;fill-column       ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       doom                ; what makes DOOM look the way it does
+       doom-dashboard      ; a nifty splash screen for Emacs
+       doom-quit           ; DOOM quit-message prompts when you quit Emacs
+       (emoji +ascii +github)  ; 🙂
+       fill-column         ; a `fill-column' indicator
+       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides     ; highlighted indent columns
-       ;;ligatures      ; ligatures and symbols to make your code pretty again
+       indent-guides       ; highlighted indent columns
+       (ligatures +extra)  ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       modeline          ; snazzy, Atom-inspired modeline, plus API
+       modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
-       ophints           ; highlight the region an operation acts on
+       ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       tabs                ; a tab bar for Emacs
+       treemacs            ; a project drawer, like neotree but cooler
+       ;; unicode          ; extended unicode support for various languages
+       vc-gutter           ; vcs diff in the fringe
+       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       workspaces          ; tab emulation, persistence & separate workspaces
+       ;; zen              ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
+       (evil +everywhere)  ; come to the dark side, we have cookies
+       file-templates      ; auto-snippets for empty files
+       fold                ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
@@ -62,21 +62,21 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
-       ;;word-wrap         ; soft wrapping with language-aware indent
+       snippets            ; my elves. They type so I don't have to
+       word-wrap           ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
-       electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       dired               ; making dired pretty [functional]
+       electric            ; smarter, keyword-based electric-indent
+       ;;ibuffer           ; interactive buffer management
+       undo                ; persistent, smarter undo for your inevitable mistakes
+       vc                  ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm               ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -85,7 +85,7 @@
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
@@ -94,7 +94,7 @@
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
-       magit             ; a git porcelain for Emacs
+       magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
@@ -119,9 +119,9 @@
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
-       (elixir +lsp)            ; erlang done right
+       (elixir +lsp)       ; erlang done right
        ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
+       emacs-lisp          ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
@@ -142,15 +142,15 @@
        ;;factor
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)    ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +pretty)            ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       ;; python           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -160,13 +160,13 @@
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       web                 ; the tubes
+       yaml                ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)
