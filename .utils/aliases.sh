40152b17098480cd86ae2be3373eea43415ac053
Vinícius Müller <vinigm.nho@gmail.com>
2021-05-24 23:21:49 -0300

    Add aliases

--- .utils/aliases.sh
+++ .utils/aliases.sh
@@ -7,8 +7,31 @@ alias nv="nvim"
 
 alias se="sudoedit"
 
+# git
+alias g="git"
 alias gs="git status"
 alias gc="git commit"
+alias gl="git log"
+alias gcl="git clone"
+alias gco="git checkout"
+alias glog="git log --oneline"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias ga="git add"
+alias gr="git remote"
+alias gp="git push"
+alias grv="git remote -v"
+alias gsh="git show"
+
+# asdf
+alias apa="asdf plugin-add"
+alias apr="asdf plugin-remove"
+alias apl="asdf plugin-list"
+alias apu="asdf plugin-update"
+alias ai="asdf install"
+alias ag="asdf global"
+alias al="asdf local"
+alias au="asdf uninstall"
 
 alias ls="exa"
 
@@ -30,6 +53,7 @@ alias pacup="sudo pacman -Syu"
 alias pacr="sudo pacman -Rns"
 alias pacf="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
 alias pacq="pacman -Q"
+alias paco="pacman -Qo"
 
 alias yays="yay -S"
 alias yayf="yay -Slq | fzf --multi --preview 'yay -Si {1}' | xargs -ro yay -S"
