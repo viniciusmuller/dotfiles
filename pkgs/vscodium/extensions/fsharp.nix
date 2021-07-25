3ff0c6fd735621e8212979bcac2d38fe7385bab8
Vinícius Müller <vinigm.nho@gmail.com>
2021-07-25 18:01:19 -0300

    Switch some vscode-extensions to nixpkgs ones

--- pkgs/vscodium/extensions/fsharp.nix
+++ pkgs/vscodium/extensions/fsharp.nix
@@ -1,17 +1,9 @@
 { pkgs, ... }:
 
-let
-  customVscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "Ionide-fsharp";
-      publisher = "Ionide";
-      version = "5.5.6";
-      sha256 = "CJf535sRmSnhJ+FZY+QUmeMbtIwqr1pFcZBjPTp4bM0=";
-    }
-  ];
-in
 {
   programs.vscode = {
-    extensions = customVscodeExtensions;
+    extensions = with pkgs.vscode-extensions; [
+      ionide.ionide-fsharp
+    ];
   };
 }
