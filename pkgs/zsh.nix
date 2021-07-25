119ec8e6496334668e31c87c8e52ea898971c998
Vinícius Müller <vinigm.nho@gmail.com>
2021-07-25 02:36:45 -0300

    Update zsh plugins

--- pkgs/zsh.nix
+++ pkgs/zsh.nix
@@ -1,11 +1,25 @@
 { pkgs, ... }:
 
 let
-  fzf-tab-completion = pkgs.fetchFromGitHub {
-    owner = "lincheney";
-    repo = "fzf-tab-completion";
-    rev = "53eb325f573265a6105c9bd0aa56cd865c4e14b7";
-    sha256 = "sha256-hvlz8/mdg9spKy2RLhqPukqdawd9+MEvW31smCsuUhA=";
+  fzf-tab-completion = {
+    name = "zsh-tab-completion";
+    file = "zsh/fzf-zsh-completion.sh";
+    src = pkgs.fetchFromGitHub {
+      owner = "lincheney";
+      repo = "fzf-tab-completion";
+      rev = "53eb325f573265a6105c9bd0aa56cd865c4e14b7";
+      sha256 = "sha256-hvlz8/mdg9spKy2RLhqPukqdawd9+MEvW31smCsuUhA=";
+    };
+  };
+  zsh-nix-shell = {
+    name = "zsh-nix-shell";
+    file = "nix-shell.plugin.zsh";
+    src = pkgs.fetchFromGitHub {
+      owner = "chisui";
+      repo = "zsh-nix-shell";
+      rev = "v0.2.0";
+      sha256 = "1gfyrgn23zpwv1vj37gf28hf5z0ka0w5qm6286a7qixwv7ijnrx9";
+    };
   };
 in
 {
@@ -46,11 +60,17 @@ in
 
       extraConfig = ''
         zstyle ':prezto:module:git:alias' skip 'yes'
-
-        . ${fzf-tab-completion}/zsh/fzf-zsh-completion.sh
-        bindkey '^I' fzf_completion
       '';
     };
+
+    plugins = [
+      zsh-nix-shell
+      fzf-tab-completion
+    ];
+
+    initExtra = ''
+      unsetopt BEEP
+    '';
 
     # TODO: Make these kind of things common between different shells #
     shellAliases = {
