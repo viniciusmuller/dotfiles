8a9d06de640b4522d60e3498642fc308907cd151
Vinícius Müller <vinigm.nho@gmail.com>
2021-08-30 11:42:33 -0300

    Move neovim init to separate file

--- pkgs/editors/nvim/default.nix
+++ pkgs/editors/nvim/default.nix
@@ -54,18 +54,6 @@ let
     endfunction
   '';
 
-  myplayground = pkgs.vimUtils.buildVimPlugin {
-    name = "playground";
-    version = "2021-07-27";
-    src = pkgs.fetchFromGitHub {
-      owner = "nvim-treesitter";
-      repo = "playground";
-      rev = "deb887b3f49d66654d9faa9778e8949fe0d80bc3";
-      sha256 = "sha256-cpSkvRRxEurF5XclAzoDSjdnyPJmxNNcUDrSO7F+VII=";
-    };
-    meta.homepage = "https://github.com/nvim-treesitter/playground/";
-  };
-
   nvim-spell-pt = builtins.fetchurl {
     url =
       "https://github.com/mateusbraga/vim-spell-pt-br/raw/3d7eb3098de77b86c8a880354b442b3d84b18a72/spell/pt.utf-8.spl";
@@ -160,7 +148,6 @@ in
     plugins = with pkgs.vimPlugins; [
       # playground # Treesitter plaground
       nvim-web-devicons
-      myplayground
       vim-polyglot
       targets-vim
       tagalong-vim
@@ -179,111 +166,7 @@ in
       let maplocalleader = ","
     '';
 
-    extraConfig = ''
-      set undofile
-      set noshowmode
-      set undolevels=1000
-      set number relativenumber
-      set expandtab tabstop=2 shiftwidth=2
-      set cursorline
-      set termguicolors
-      set colorcolumn=80 " Ruler
-      set nofoldenable
-      set showcmd
-      set ignorecase smartcase
-      set textwidth=80
-      set sessionoptions+=globals
-      set hidden
-      set guifont=JetBrains\ Mono:h12
-      set wildignorecase
-      set linebreak
-      set autoindent
-      set smartindent
-      set splitright
-      set scrolloff=5
-      set lazyredraw
-      set noswapfile
-      set nomodeline
-      set autoread
-      set completeopt=menuone,noselect
-      set pumheight=10 " Max number of items in autocompletion popup
-      set pumwidth=25
-      set updatetime=400
-      " Some plugin is removing `-` from the separators, for now lets just get it back.
-      set iskeyword-=-
-      " Don't auto line break when inserting text
-      set formatoptions-=t
-      set shortmess+=I
-      " set listchars=tab:»\ ,space:·,eol:¬
-      " set list
-
-      noremap Y "+y
-      noremap H ^
-      noremap L $
-      nnoremap Q @@
-
-      nnoremap <C-q> <C-w>q
-      " nnoremap <C-s> <cmd>update<cr>
-
-      " -- Quickfix/Location lists --
-      command Cnext try | cnext | catch | cfirst | catch | endtry
-      command Cprev try | cprev | catch | clast  | catch | endtry
-      command Lnext try | lnext | catch | lfirst | catch | endtry
-      command Lprev try | lprev | catch | llast  | catch | endtry
-
-      nnoremap [q <cmd>Cprev<cr>
-      nnoremap ]q <cmd>Cnext<cr>
-      nnoremap [Q <cmd>cfirst<cr>
-      nnoremap ]Q <cmd>clast<cr>
-
-      nnoremap [w <cmd>Lprev<cr>
-      nnoremap ]w <cmd>Lnext<cr>
-      nnoremap [W <cmd>lfirst<cr>
-      nnoremap ]W <cmd>llast<cr>
-
-      " Tabs
-      nnoremap <leader>to :tabnew<space>
-      nnoremap <leader>tq :tabclose<cr>
-      nnoremap <silent>g< :tabmove tabpagenr() - 2<cr>
-      nnoremap <silent>g> :tabmove tabpagenr() + 1<cr>
-
-      nnoremap <silent> <leader>vQ <cmd>quitall!<cr>
-      nnoremap <silent> <leader>vq <cmd>quitall<cr>
-      nnoremap <silent> <leader>vr <cmd>source $MYVIMRC<cr>
-
-      augroup my_autocommands
-        " Remove trailing whitespaces on write
-        au BufWritePre * %s/\s\+$//e
-        " Open help windows vertically splitted
-        au FileType help wincmd L
-        " Highlight on yank (nvim only)
-        au TextYankPost * silent! lua vim.highlight.on_yank{higroup="HighlightedYankRegion", timeout=50}
-      augroup end
-
-      let g:autosave_autostart = 1
-      function s:autosave()
-        let autosave_blacklist = ['NvimTree', 'help' ]
-        if index(autosave_blacklist, &ft) < 0 && w:autosave == 1 && &modifiable
-          silent update
-        endif
-      endfunction
-
-      augroup myautosave
-        autocmd InsertLeave,TextChanged * call s:autosave()
-        autocmd BufWinEnter * let w:autosave = g:autosave_autostart
-      augroup end
-
-      command ToggleAutoSave let w:autosave = w:autosave ? 0 : 1
-
-      function! SynStack()
-        if !exists("*synstack")
-          return
-        endif
-        echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
-      endfunc
-
-      nnoremap <leader>q :call SynStack()<cr>
-    '';
+    extraConfig = builtins.readFile ./init.vim;
   };
 
   # inherit (prelude.mkShellAlias aliases);
---
+++ pkgs/editors/nvim/init.vim
@@ -0,0 +1,94 @@
+set undofile
+set noshowmode
+set undolevels=1000
+set number relativenumber
+set expandtab tabstop=2 shiftwidth=2
+set cursorline
+set termguicolors
+set colorcolumn=80 " Ruler
+set nofoldenable
+set showcmd
+set ignorecase smartcase
+set textwidth=80
+set sessionoptions+=globals
+set hidden
+set guifont=JetBrains\ Mono:h12
+set wildignorecase
+set linebreak
+set autoindent
+set smartindent
+set splitright
+set scrolloff=5
+set lazyredraw
+set noswapfile
+set nomodeline
+set autoread
+set completeopt=menuone,noselect
+set pumheight=10 " Max number of items in autocompletion popup
+set pumwidth=25
+set updatetime=400
+" Some plugin is removing `-` from the separators, for now lets just get it back.
+set iskeyword-=-
+" Don't auto line break when inserting text
+set formatoptions-=t
+set shortmess+=I
+" set listchars=tab:»\ ,space:·,eol:¬
+" set list
+
+noremap Y "+y
+noremap H ^
+noremap L $
+nnoremap Q @@
+
+nnoremap <C-q> <C-w>q
+" nnoremap <C-s> <cmd>update<cr>
+
+" -- Quickfix/Location lists --
+command Cnext try | cnext | catch | cfirst | catch | endtry
+command Cprev try | cprev | catch | clast  | catch | endtry
+command Lnext try | lnext | catch | lfirst | catch | endtry
+command Lprev try | lprev | catch | llast  | catch | endtry
+
+nnoremap [q <cmd>Cprev<cr>
+nnoremap ]q <cmd>Cnext<cr>
+nnoremap [Q <cmd>cfirst<cr>
+nnoremap ]Q <cmd>clast<cr>
+
+nnoremap [w <cmd>Lprev<cr>
+nnoremap ]w <cmd>Lnext<cr>
+nnoremap [W <cmd>lfirst<cr>
+nnoremap ]W <cmd>llast<cr>
+
+" Tabs
+nnoremap <leader>to :tabnew<space>
+nnoremap <leader>tq :tabclose<cr>
+nnoremap <silent>g< :tabmove tabpagenr() - 2<cr>
+nnoremap <silent>g> :tabmove tabpagenr() + 1<cr>
+
+nnoremap <silent> <leader>vQ <cmd>quitall!<cr>
+nnoremap <silent> <leader>vq <cmd>quitall<cr>
+nnoremap <silent> <leader>vr <cmd>source $MYVIMRC<cr>
+
+augroup my_autocommands
+  " Remove trailing whitespaces on write
+  au BufWritePre * %s/\s\+$//e
+  " Open help windows vertically splitted
+  au FileType help wincmd L
+  " Highlight on yank (nvim only)
+  au TextYankPost * silent! lua vim.highlight.on_yank{higroup="HighlightedYankRegion", timeout=50}
+augroup end
+
+let g:autosave_autostart = 1
+function s:autosave()
+  let autosave_blacklist = ['NvimTree', 'help' ]
+  if index(autosave_blacklist, &ft) < 0 && w:autosave == 1 && &modifiable
+    silent update
+  endif
+endfunction
+
+augroup myautosave
+  autocmd InsertLeave,TextChanged * call s:autosave()
+  autocmd BufWinEnter * let w:autosave = g:autosave_autostart
+augroup end
+
+command ToggleAutoSave let w:autosave = w:autosave ? 0 : 1
