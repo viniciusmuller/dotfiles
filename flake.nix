c21e89ef87ed5248040a453cf08018b53cb245a5
Vinícius Müller <vinigm.nho@gmail.com>
2021-09-17 14:59:32 -0300

    Update devshell and flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,29 @@
 {
-  description = "A very basic flake";
+  description = "Personal flake config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:arcticlimer/home-manager/add-neovim-initextra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # TODO: Fix the flake on the suckless repository and use its package overrides
-    suckless.url = "github:arcticlimer/suckless";
-
     emacs-overlay.url = "github:nix-community/emacs-overlay/master";
     nix-doom-emacs.url = "github:vlaci/nix-doom-emacs/master";
     nix-doom-emacs.inputs.emacs-overlay.follows = "emacs-overlay";
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-doom-emacs, ... } @inputs:
+  outputs = { self, flake-utils, devshell, nixpkgs, home-manager, nix-doom-emacs, ... } @inputs:
     let
       overlays = with inputs; [
         # TODO: Overlays doesn't seem to be working
-        suckless.overlays
         emacs-overlay.overlay
       ];
 
@@ -29,69 +31,92 @@
       mkPkgs = pkgs: extraOverlays:
         import pkgs { inherit system; };
 
-      pkgs = mkPkgs nixpkgs [ ];
+      pkgs = mkPkgs nixpkgs [];
+      lib = nixpkgs.lib;
 
       prelude = {
         mkLuaCode =
-          (code:
-            ''
-              lua << EOF
-                ${code}
-              EOF
-            '');
+          (
+            code:
+              ''
+                lua << EOF
+                  ${code}
+                EOF
+              ''
+          );
 
-        mkShellAlias = (alias: {
-          programs.zsh.shellAliases = alias;
-          programs.bash.shellAliases = alias;
-          programs.fish.shellAliases = alias;
-        });
+        mkShellAlias = (
+          alias: {
+            programs.zsh.shellAliases = alias;
+            programs.bash.shellAliases = alias;
+            programs.fish.shellAliases = alias;
+          }
+        );
       };
     in
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        modules = [
-          { nixpkgs.overlays = overlays; }
-          ./hosts/nixos
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.vini = { ... }: {
-              imports = [ nix-doom-emacs.hmModule ];
-              _module.args = {
-                inherit prelude;
+      {
+        nixosConfigurations.nixos = lib.nixosSystem rec {
+          system = "x86_64-linux";
+          modules = [
+            { nixpkgs.overlays = overlays; }
+            ./hosts/nixos
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.users.vini = { ... }: {
+                imports = [ nix-doom-emacs.hmModule ];
+                _module.args = {
+                  inherit prelude;
+                };
               };
-            };
+            }
+          ];
+          specialArgs = {
+            # We pass prelude here
+            inherit inputs system prelude;
+          };
+        };
+
+        homeConfigurations = {
+          arch = home-manager.lib.homeManagerConfiguration {
+            configuration = ./hosts/arch;
+            system = "x86_64-linux";
+            homeDirectory = "/home/vini";
+            username = "vini";
+            extraSpecialArgs = { inherit prelude; };
+          };
+
+          wsl = home-manager.lib.homeManagerConfiguration {
+            configuration = ./hosts/wsl;
+            system = "x86_64-linux";
+            homeDirectory = "/home/vini";
+            username = "vini";
+            extraSpecialArgs = { inherit prelude; };
+          };
+        };
+        # Devshell
+      } // flake-utils.lib.eachDefaultSystem
+        (
+          system: {
+            devShell =
+              let
+                pkgs = import nixpkgs {
+                  inherit system;
+                  overlays = [ devshell.overlay ];
+                };
+              in
+                pkgs.devshell.mkShell {
+                  imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+                };
           }
-        ];
-        specialArgs = {
-          # We pass prelude here
-          inherit inputs system prelude;
-        };
-      };
+        );
+    }
 
-      homeConfigurations = {
-        arch = home-manager.lib.homeManagerConfiguration {
-          configuration = ./hosts/arch;
-          system = "x86_64-linux";
-          homeDirectory = "/home/vini";
-          username = "vini";
-          extraSpecialArgs = { inherit prelude; };
-        };
-
-        wsl = home-manager.lib.homeManagerConfiguration {
-          configuration = ./hosts/wsl;
-          system = "x86_64-linux";
-          homeDirectory = "/home/vini";
-          username = "vini";
-          extraSpecialArgs = { inherit prelude; };
-        };
-      };
-
-      devShell."${system}" = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nixpkgs-fmt
-          rnix-lsp
-        ];
-      };
-    };
-}
+# {
+#   devShell."${system}" = pkgs.mkShell {
+#     buildInputs = with pkgs; [
+#       nixpkgs-fmt
+#       rnix-lsp
+#     ];
+#   };
+# }
+# }
