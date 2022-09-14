ee76b81f1aa8718e8cb23e02d51caf3952f6ee5d
Vinícius Müller <vinigm.nho@gmail.com>
2022-09-14 16:20:22 -0300

    chore: remove unused set merging on templates

--- templates/basic/flake.nix
+++ templates/basic/flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    { } // utils.lib.eachDefaultSystem (system:
+    utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
